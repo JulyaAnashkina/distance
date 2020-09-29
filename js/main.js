1615f7adc2dbc26f5d1e9d7c9a41f3f45e3acf1c
JulyaAnashkina <julyaanashkin@yandex.ru>
2020-09-29 16:01:27 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 const menuBtn = document.querySelector('.m-menu');
-const menu=document.querySelector('.menu-sidebar');
+const menu = document.querySelector('.menu-sidebar');
+const mainFeed = document.querySelector('body');
+
+const radioButtons = document.querySelectorAll('.radio-button');
+const fanpageGroup = [
+    document.querySelector('.fanpage-likes'),
+    document.querySelector('.fanpage-views'),
+    document.querySelector('.fanpage-posts')
+];
 
 menuBtn.addEventListener('click', function () {
     menu.classList.toggle('active');
-})
+    mainFeed.classList.toggle('no-scroll');
+});
+
+radioButtons.forEach((radio, i) => radio.addEventListener('click', () => {
+    fanpageGroup.forEach(fanpage => {
+        fanpage.classList.remove('active');
+    });
+    fanpageGroup[i].classList.add('active');
+}));
